ef3283e22c206b0a71ceb97f0214a1d20930f040
saquibsayyedcoder <saquibsayyed1345@gmail.com>
2025-12-16 15:18:59 +0530

    working the api of snippets

--- snipts/index.js
+++ snipts/index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use("api/v1/snippets", snippetRouter);
+app.use("/api/v1/snippets", snippetRouter);
 
 app.listen(PORT, () => {
   console.log(`Snipts Microservice is running on http://localhost:${PORT}`);
--- snipts/routes/snippetRoutes.js
+++ snipts/routes/snippetRoutes.js
@@ -3,7 +3,7 @@ import { createSnippet, getSnippet } from '../controller/snippet.controller.js';
 
 const router = expresss.Router();
 
-router.route("/").post(createSnippet);
+router.route("/create").post(createSnippet);
 router.route("/").get(getSnippet);
 
 export default router;
