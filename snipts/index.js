df39075dc1174752e8e03ad1866a1fa169542032
saquibsayyedcoder <saquibsayyed1345@gmail.com>
2025-12-17 18:28:01 +0530

    changes in snippets code

--- snipts/index.js
+++ snipts/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import snippetRouter from './routes/snippetRoutes.js';
+import cors from "cors"
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors ({
+  origin:"http://localhost:5173"
+}))
 
 app.use("/api/v1/snippets", snippetRouter);
 
